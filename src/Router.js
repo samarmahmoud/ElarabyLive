58e2aaf07bf8337773b0ac15f83230d18856d8c8
samarMahmoud <samarabdelkhalek812@yahoo.com>
2018-08-14 17:39:07 +0200

    fix Icon display

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Image,Text} from 'react-native';
 import {Router,Scene,Tabs} from 'react-native-router-flux';
 import Splash from './components/Splash';
 import Login from './components/Login';
@@ -10,15 +10,31 @@ import gallery from './assets/gallery.png';
 import HQ from './assets/hq.png';
 import Twasol from './assets/tawasol.png';
 
-const IconComponent()=>{
-    return(
-        <Image source={profileIcon} style={{width:22,height:23}}/>
-    );
+const IconComponent=(props)=>{
+
+   switch(props.name){
+       case "profile":
+       return (<Image source={profileIcon} style={{width:22, height:23}}/>)
+       break;
+       case "main":
+       return (<Image source={main} style={{width:22, height:23}}/>)
+       break;
+       case "twasol":
+       return (<Image source={Twasol} style={{width:22, height:23}}/>)
+       break;
+       case "_gallery":
+       return (<Image source={gallery} style={{width:22, height:23}}/>)
+       break;
+       case "_HQ":
+       return (<Image source={HQ} style={{width:22, height:23}}/>)
+       break;
+       default :
+       return (<Text>Error</Text>) ;
+   }
     
-}
+};
 
 const Router_Component=()=>{
-    // let iconSize={{width:22, height:23}};
     return(
         <Router>
             <Scene key="root" hideNavBar>
@@ -27,13 +43,16 @@ const Router_Component=()=>{
               </Scene>
              <Scene key="auth">
                <Scene key="Login" component={Login} hideNavBar  type='rest' />
-               
+                
              </Scene>    
              <Scene key="NewFeed">
-             <Tabs tabBarPosition='bottom' showLabel={false} showIcon={true} activeBackgroundColor="#25abf9">
-             <Scene key='Profile'  component={Home}  Icon={{IconComponent}}   hideNavBar  />
-             <Scene key='Twasol'  component={Login}  Icon={Twasol} hideNavBar/>
-             <Scene key='Gallery'  component={Home}  Icon={gallery} hideNavBar/>
+              <Tabs tabBarPosition='bottom' showLabel={false} showIcon={true} activeBackgroundColor="#155da3">
+               <Scene key='Profile'  component={Home} icon={IconComponent} name="profile"  hideNavBar />
+
+               <Scene key='Twasol'  component={Login} icon={IconComponent} name="twasol"  hideNavBar/>
+               <Scene key='Gallery' component={Home}  icon={IconComponent} name="_gallery"  hideNavBar/>
+               <Scene key='Hq'      component={Home}  icon={IconComponent} name="_HQ"  hideNavBar/>
+               <Scene key='main'    component={Home}  icon={IconComponent} name="main"  hideNavBar/>
              </Tabs>
              {/* <Scene key="Home" component={Home} hideNavBar  type='rest'/> */}
              </Scene>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {View,Text} from 'react-native';
+import {Header} from './common';
 
 export default class Home extends React.Component{
     render(){
         return(
             <View>
-                <Text>
-                    Home Screen
-                </Text>
+                <Header headerTitle=""/>
             </View>
         );
     }
